8d80bbc9fc4db5f207463dc4ac994eb6070c1e8b
Luc <Lucparguel12720@gmail.com>
2025-04-01 21:31:06 +0200

    UserControllerAuth

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RootBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MeController : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        var identity = User.Identity;
+        if (identity == null || !identity.IsAuthenticated)
+        {
+            return Unauthorized();
+        }
+
+        var claims = User.Claims.ToDictionary(c => c.Type, c => c.Value);
+        return Ok(new
+        {
+            IsAuthenticated = identity.IsAuthenticated,
+            Name = identity.Name,
+            Claims = claims
+        });
+    }
+}
